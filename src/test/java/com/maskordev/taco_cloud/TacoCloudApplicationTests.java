f29b331b1c2013549025454a8d2ef023e41b7fb4
TrueXz <maskor.gpt@gmail.com>
2025-06-28 11:54:21 +0500

    Initialize project

---
+++ src/test/java/com/maskordev/taco_cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maskordev.taco_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
