2a34b01c8e6f01846074929f6dbf41c8c031a424
TrueXz <maskor.gpt@gmail.com>
2025-06-28 15:09:13 +0500

    Добавил парочку контроллеров

---
+++ src/main/java/com/maskordev/taco_cloud/Taco.java
@@ -0,0 +1,18 @@
+package com.maskordev.taco_cloud;
+
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.List;
+
+@Data
+public class Taco {
+    @NonNull
+    @Size(min=5, message="Name must be at least 5 characters long")
+    private String name;
+
+    @NonNull
+    @Size(min=1, message="You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
+}
